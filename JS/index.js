8dfe0cf42c3bb873a9f884ebd45ab1275fba598f
Alexhoyosf <alexhoyosf080302@gmail.com>
2023-04-08 18:55:59 -0600

    Cambios a la galeria parallax

--- JS/index.js
+++ JS/index.js
@@ -12,16 +12,18 @@ let header = document.getElementById('galery');
 let coords = header.getBoundingClientRect();
 console.log(coords.top)
 window.addEventListener('scroll', function(){
-    let value = parseFloat(window.scrollY-coords.top);
-    gotas.style.top = value * 1.05 + 'px';
-    triangulo.style.left = value * -0.45 + 'px';
-    main.top = value * 0.5 + 'px';
-    main_front.style.left = value * -2 + 'px';
-    main_front2.style.left = value * 4 + 'px';
-    text.style.marginRight = value * -5 + 'px';
-    text.style.marginTop = value * 0.7 + 'px';
-    btn.style.marginTop = value * 0.7 + 'px';
-    title.style.marginRight = value * 7 + 'px';
-    title.style.marginTop = value * 0.7 + 'px';
-    //header.style.top = value * -0.35 + 'px';
+    if(coords.top <= this.window.scrollY){
+        let value = parseFloat(window.scrollY-coords.top);
+        gotas.style.top = value * 1.05 + 'px';
+        triangulo.style.left = value * -0.45 + 'px';
+        main.top = value * 0.5 + 'px';
+        main_front.style.left = value * -2 + 'px';
+        main_front2.style.left = value * 4 + 'px';
+        text.style.marginRight = value * -7 + 'px';
+        text.style.marginTop = value * 0.7 + 'px';
+        btn.style.marginTop = value * 1 + 'px';
+        title.style.marginRight = value * 9 + 'px';
+        title.style.marginTop = value * 0.7 + 'px';
+        //header.style.top = value * -0.35 + 'px';
+    }
 })
